8aac0cc0d66c3bdf576e3384b0a7c5e442368678
James Grover <jgrovermail@gmail.com>
2019-05-11 13:24:38 +0000

    Initial react layout serving srkn logo.  contact remnants remain

--- src/js/views/Contacts.jsx
+++ src/js/views/Contacts.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 import ContactCard from '../components/ContactCard';
 import Modal from '../components/Modal';
 import { Context } from "../store/appContext.jsx";
@@ -20,33 +19,16 @@ export default class Contacts extends React.Component {
 		<Context.Consumer>
 			{({ store, actions }) => {
 				
-				return store.contacts.map((item, index) => {
+			//	return store.contacts.map((item, index) => {
 					//let id = this.props.id;
 					//console.log(id);
 					return (
-						<div className="container" key={index}>
-							<div>
-								<p className="text-right my-3">
-									<Link className="btn btn-success" to="/add">Add new contact</Link>
-								</p>
-								<div id="contacts" className="panel-collapse collapse show" aria-expanded="true">
-									<ul className="list-group pull-down" id="contact-list">
-										<ContactCard id={index} key={index} onDelete={() =>
-											
-											//alert(id)
-											
-											this.setState({ showModal: true, modalId: index })
-											
-										}/>
-										
-									</ul>
-								</div>
-							</div>
-							<Modal id={index} show={this.state.showModal} onClose={() => this.setState({showModal: false})} />
-						
+						<div>
+						<h1><a href="index.html"><img src={require("../../images/logo.png")} alt="Logo"></img></a></h1>
 						</div>
+						
 					);
-				});
+				// commented out to match commented out array mapping  });
 					}}
 		
 		</Context.Consumer>
