ba1224523b0d27038e57100e3f058d5068be42c6
James Grover <jgrovermail@gmail.com>
2019-05-11 14:01:09 +0000

    updated layout to point to DashboardHome.  Left componentcard remnants as model for item cards

--- src/js/Layout.jsx
+++ src/js/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Store from "./store/appContext.jsx";
-import Contacts from "./views/Contacts.jsx";
+import DashboardHome from "./views/DashboardHome.js";
 import EditContact from "./views/EditContact.jsx";
 import AddContact from "./views/AddContact.jsx";
 
@@ -12,9 +12,9 @@ class Layout extends React.Component {
 				<BrowserRouter>
 					<div>
 						<Switch>
-							<Route exact path="/index.html" component={Contacts} />
-							<Route exact path="/" component={Contacts} />
-							<Route exact path="/contacts" component={Contacts} />
+							<Route exact path="/index.html" component={DashboardHome} />
+							<Route exact path="/" component={DashboardHome} />
+							<Route exact path="/contacts" component={DashboardHome} />
 							<Route exact path="/add" component={AddContact} />
 							<Route exact path="/edit/:id" component={EditContact} />
 							<Route render={() => <h1 className="notfound">Not found!</h1>} />
